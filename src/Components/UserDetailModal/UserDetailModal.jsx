b9c204bbf13eb2f409252de9a9f06e9cb8429d36
Saurabh <nirmalesaurabh99@gmail.com>
2024-09-03 16:45:33 +0530

    first commit

--- src/Components/UserDetailModal/UserDetailModal.jsx
+++ src/Components/UserDetailModal/UserDetailModal.jsx
@@ -1,4 +1,4 @@
-import "./UserDetailModal.css"
+import "./UserDetailModal.css";
 
 const UserDetailModal = ({ isOpen, onClose, user }) => {
   if (!isOpen || !user) return null;
@@ -10,11 +10,42 @@ const UserDetailModal = ({ isOpen, onClose, user }) => {
           &times;
         </button>
         <h2>User Details</h2>
-        <p><strong>Name:</strong> {user.first} {user.last}</p>
-        <p><strong>Landmark:</strong> {user.landmark}</p>
-        <p><strong>Email:</strong> {user.email}</p>
-        <p><strong>About:</strong> {user.aboutus}</p>
-        {/* Add other user details as needed */}
+        <p>
+          <strong>Name:</strong> {user.first} {user.middle} {user.last}
+        </p>
+        <p>
+          <strong>Landmark:</strong> {user.landmark}
+        </p>
+        <p>
+          <strong>Email:</strong> {user.email}
+        </p>
+        <p>
+          <strong>Contact:</strong> {user.contact}
+        </p>
+        <p>
+          <strong>About:</strong> {user.aboutus}
+        </p>
+        {user.otherLinks && user.otherLinks.length > 0 && (
+          <div>
+            <strong>Other Links:</strong>
+            <ul>
+              {user.otherLinks.map((link, index) => (
+                <li key={index}>
+                  <strong>{link.title}</strong> - {link.urlLink}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <p>
+          <strong>Shareable Link:</strong> {user.shareableLink}
+        </p>
+        {user.profileimg && (
+          <div>
+            <strong>Profile Image:</strong>
+            <img src={user.profileimg} alt="Profile" style={{ width: "100px", height: "100px" }} />
+          </div>
+        )}
       </div>
     </div>
   );
